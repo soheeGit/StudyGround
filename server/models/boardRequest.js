51ed8fd30bea0827230570616ce2188972406992
soheeGit <63wlsthgml@gmail.com>
2024-07-08 02:48:57 +0900

    오류해결

--- server/models/boardRequest.js
+++ server/models/boardRequest.js
@@ -1,10 +1,9 @@
 const Sequelize = require('sequelize');
-const { sequelize } = require('./models');
 
 class BoardRequest extends Sequelize.Model {
     static initiate(sequelize) {
         BoardRequest.init({
-            bqid: {
+            id: {
                 type: Sequelize.INTEGER,
                 autoIncrement: true,
                 primaryKey: true,
@@ -13,18 +12,10 @@ class BoardRequest extends Sequelize.Model {
             boardId: {
                 type: Sequelize.INTEGER,
                 allowNull: false,
-                references: {
-                    model: 'boards',
-                    key: 'bId'
-                }
             },
             userId: {
                 type: Sequelize.INTEGER,
                 allowNull: false,
-                references: {
-                    model: 'users',
-                    key: 'id'
-                }
             },
             status: {
                 type: Sequelize.ENUM('pending', 'accepted', 'rejected'),
@@ -40,6 +31,10 @@ class BoardRequest extends Sequelize.Model {
             charset: 'utf8mb4',
             collate: 'utf8mb4_general_ci',
         });
+    }
+    static associate(db) {
+        db.BoardRequest.belongsTo(db.Board, { foreignKey: 'boardId', targetKey: 'bId' });
+        db.BoardRequest.belongsTo(db.User, { foreignKey: 'userId', targetKey: 'id' });
     }
 }
 
