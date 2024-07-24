892942d32073e92546f2dac50da2d770d4f8c02a
soheeGit <63wlsthgml@gmail.com>
2024-07-25 02:45:47 +0900

    테스트 대비 코드 수정

--- server/models/index.js
+++ server/models/index.js
@@ -15,7 +15,7 @@ const basename = path.basename(__filename)
 fs
   .readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js')
+    return (file.indexOf('.') !== 0) && !file.includes('test') && (file !== basename) && (file.slice(-3) === '.js')
   })
   .forEach(file => {
     const model = require(path.join(__dirname, file))
