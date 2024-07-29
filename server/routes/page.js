fba4d75a805f920b0605667c636dce81326ff4de
soheeGit <63wlsthgml@gmail.com>
2024-07-29 15:53:21 +0900

    비밀번호 넘기지 않게 수정, 스터디 수락 후 입장 데이터 추가

--- server/routes/page.js
+++ server/routes/page.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('../middlewares');
-const { getBoardData, getMyBoardData, partBoardsData, postBoardData, postApplyBoard, postAcceptBoard, postRejectBoard } = require('../controllers/page');
+const { getBoardData, getMyBoardData, partBoardsData, postBoardData, postApplyBoard, postAcceptBoard, postRejectBoard, acceptBoardEnter } = require('../controllers/page');
 const router = express.Router();
 
 // 스터디 조회
@@ -23,5 +23,8 @@ router.post('/accept-board/:requestId', isLoggedIn, postAcceptBoard)
 
 // 스터디 신청 거절
 router.post('/reject-board/:requestId', isLoggedIn, postRejectBoard)
+
+// 수락된 스터디 입장
+router.get('/acceptBoardEnter/:id', isLoggedIn, acceptBoardEnter)
 
 module.exports = router;
