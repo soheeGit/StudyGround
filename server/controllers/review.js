61bc59024c5534a0ea52105821f3a259867c8d7f
soheeGit <63wlsthgml@gmail.com>
2024-08-21 14:14:52 +0900

    리뷰하기전 팀원이름 조회(본인제외)

--- server/controllers/review.js
+++ server/controllers/review.js
@@ -67,3 +67,34 @@ exports.getReviewData = async (req, res) => {
         res.status(500).json({ error: '리뷰 데이터 조회 실패' });
     }
 };
+
+exports.getName = async (req, res) => {
+    const reviewerId = req.user.id;
+    const boardId = req.params.id;
+    try {
+        const user = await User.findOne({ where: {id: reviewerId} });
+        if( !user ){
+            return res.status(404).json({ error: '사용자를 찾을 수 없습니다.' });
+        };
+        const board = await Board.findByPk(boardId, {
+            include: [{
+                model: User,
+                attributes: ['id', 'uId', 'uEmail', 'uName'],
+                through: { attributes: [] },    // 중간 테이블의 모든 필드를 제외
+            }]
+        });
+        if( !board ){
+            return res.status(404).json({ error: '스터디를 찾을 수 없습니다.' });
+        };
+        const boarduser = await board.hasUser(user);
+        if( !boarduser ) {
+            return res.status(404).json({ error: '해당 스터디에 참여하지 않습니다.' });
+        }
+        const filteredUsers = board.Users.filter(u => u.id !== reviewerId);
+
+        return res.status(200).json({ users: filteredUsers });
+    } catch(error) {
+        console.error(error);
+        res.status(500).json({ error: '서버오류' });
+    }
+};
--- server/routes/review.js
+++ server/routes/review.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('../middlewares');
-const { submitReview, getReviewData } = require('../controllers/review');
+const { submitReview, getReviewData, getName } = require('../controllers/review');
 const router = express.Router();
 
 // 스터디 리뷰 등록
 router.post('/submit/:id', isLoggedIn, submitReview);
 // 내가 받은 모든 리뷰 조회
-router.get('/', isLoggedIn, getReviewData)
+router.get('/', isLoggedIn, getReviewData);
+// 리뷰 등록하기 전, 팀원 아이디와 이름 조회
+router.get('/getName/:id', isLoggedIn, getName);
 
 module.exports = router;
