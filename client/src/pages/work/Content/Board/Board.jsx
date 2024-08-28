8c7c89607ee9f122508c4effc7129b33dffc8f67
EOK00 <himsim00@gmail.com>
2024-08-28 10:24:00 +0900

    임시 whiteboard 추가

--- client/src/pages/work/Content/Board/Board.jsx
+++ client/src/pages/work/Content/Board/Board.jsx
@@ -1,6 +1,53 @@
+import styled from 'styled-components';
 import WorkHeader from '../../WorkHeader';
+import {
+  Excalidraw,
+  serializeAsJSON,
+  restoreFromJSON,
+} from '@excalidraw/excalidraw';
+import { useEffect, useRef } from 'react';
+import { Button } from '../../Component/Button';
 
 const Board = () => {
-  return <WorkHeader title="Board" />;
+  const excalidrawRef = useRef(null);
+
+  const saveToLocalStorage = () => {
+    if (excalidrawRef.current) {
+      console.log('hhah');
+      const data = serializeAsJSON(excalidrawRef.current.getSceneElements());
+      localStorage.setItem('excalidrawContent', data);
+    }
+  };
+
+  const loadFromLocalStorage = () => {
+    const savedData = localStorage.getItem('excalidrawContent');
+    if (savedData) {
+      const elements = restoreFromJSON(savedData).elements;
+      excalidrawRef.current.updateScene({ elements });
+    }
+  };
+
+  useEffect(() => {
+    loadFromLocalStorage();
+  }, []);
+
+  return (
+    <>
+      <WorkHeader title="Board" />
+      <Wrap>
+        <Excalidraw ref={excalidrawRef} />
+        {/* <button onClick={saveToLocalStorage}>저장</button> */}
+      </Wrap>
+    </>
+  );
 };
 export default Board;
+
+const Wrap = styled.div`
+  width: 100%;
+  height: 80vh;
+  margin-top: 10px;
+  margin-bottom: auto;
+  margin-left: auto;
+  margin-right: auto;
+`;
